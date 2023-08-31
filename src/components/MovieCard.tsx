5abb3bfbb0e445ec852ef4261b2260bcc198f1e6
Bram van Dijke <9020733@student.zadkine.nl>
2023-08-31 12:06:41 +0200

    Details page deels functioneel

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 
 type Props = {
   movie: IMDBMovie;
-
 }
 
 const MovieCard: FunctionComponent<Props> = ({ movie }) => {
@@ -13,10 +12,9 @@ const MovieCard: FunctionComponent<Props> = ({ movie }) => {
       <div className="flex-1 flex flex-col">
         <img className="h-48 mx-auto mt-4" src={movie.Poster} alt={movie.Title} />
         <div className="p-4">
-          <Link to={`/movies/${movie.Title}`}>
+          <Link to={`/movies/${movie.imdbID}`}>
           <h3 className="mt-6 text-gray-900 text-sm font-medium">{movie.Title}</h3>
           </Link>
-          <h3 className="mt-6 text-gray-900 text-sm font-medium">{movie.Title}</h3>
           <dl className="mt-1 flex-grow flex flex-col justify-between">
             <dd className="text-gray-500 text-sm">{movie.Year}</dd>
           </dl>
--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,32 +1,54 @@
-import { FunctionComponent, useEffect } from 'react';
+import { FunctionComponent, useState, useEffect } from 'react';
 import Toggle from '../components/Toggle';
 import { useParams } from 'react-router-dom';
+import { IMDBMovie } from '../model/movie';
 
-const Detail: FunctionComponent = () => {
+type Props = {
+  movieDetail: IMDBMovie;
+}
+
+const Detail: FunctionComponent = (movieDetail) => {
+  const [movie, setMovie] = useState<IMDBMovie | null>(null);
+  const params = useParams();
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const result = await fetch(`http://www.omdbapi.com/?apikey=1a993ee0&i=tt1285016`);
+      console.log(result); // Log the response object
+      const data = await result.json();
+  
+      console.log(data); // Log the data
+  
+      setMovie(data);
+    };
+    getMovie();
+  }, [params.imdbID]);
+
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
           <div>
             <div className="w-full aspect-w-1 aspect-h-1">
-              <img className="w-full h-full object-center object-cover sm:rounded-lg" />
+              <img className="w-full h-full object-center object-cover sm:rounded-lg" src={movie?.Poster}
+/>
             </div>
           </div>
           <div className="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
             <Toggle />
             <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
-              Movie title
+              {movie?.Title}
             </h1>
             <div className="mt-3">
-              <p className="text-3xl text-gray-900">Year</p>
+              <p className="text-3xl text-gray-900">{movie?.Year}</p>
             </div>
             <div className="mt-3">
-              <p className="text-xl text-gray-900">Actors</p>
+              <p className="text-xl text-gray-900">{movie?.Actors}</p>
             </div>
             <div className="mt-6">
               <h3 className="sr-only">Description</h3>
               <div className="text-base text-gray-700 space-y-6">
-                <p>Plot</p>
+                <p>{movie?.Plot}</p>
               </div>
             </div>
             <div className="mt-8 flex justify-between">{/* link back */}</div>
@@ -35,6 +57,6 @@ const Detail: FunctionComponent = () => {
       </div>
     </div>
   );
-};
+};  
 
 export default Detail;
